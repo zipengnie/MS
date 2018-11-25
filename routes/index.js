a1d2d01f275d15f80a6989681b5f9dfeaf6d8d12
zipengnie <zipengnie@outlook.com>
2018-11-25 10:55:33 +0800

    完成了管理系统的用户管理界面

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,19 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+/* 用户管理页面*/ 
+router.get("/userInfo.html",function(req,res){
+  res.render("userInfo",{ title: ' 用户管理页面' })
+})
+
+/* 品牌管理页面*/ 
+router.get("/brand.html",function(req,res){
+  res.render("brand",{ title: ' 品牌页面' })
+})
+
+/* 品牌管理页面*/ 
+router.get("/phone.html",function(req,res){
+  res.render("phone",{ title: '手机页面' })
+})
 
 module.exports = router;
